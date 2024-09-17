08b81ed7e3da4f92b1bd7a83d642e0f12cbcbbda
wsh-n <dev@wshin.me>
2024-09-17 19:48:36 +0900

    [#1] refactor: 주석 추가

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,14 @@
 const config = {
+  /**
+   * 블로그 메타 정보
+   */
   meta: {
     blogTitle: "wsh-n",
     blogDescription: "Hello, world!",
   },
+  /**
+   * 네비게이션 바에 노출할 링크 목록
+   */
   navLinks: [
     { name: "Posts", url: "/" },
     { name: "Archive", url: "/archive" },
