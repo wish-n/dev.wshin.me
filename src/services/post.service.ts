447c09336a2278972edda393763d43683cf3af44
wsh-n <dev@wshin.me>
2024-10-05 17:37:59 +0900

    [#7] archive 페이지 제거

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -5,25 +5,6 @@ class PostService extends MarkdownContentService<Post> {
   constructor() {
     super("contents/posts");
   }
-
-  /**
-   * 모든 포스트를 연도별 GROUP BY 형태로 조회
-   * (연도 최신순, 연도 내 포스트들은 작성일시 최신순)
-   */
-  getAllGroupByYear(): { year: number; posts: Post[] }[] {
-    const mapByYear = new Map<number, Post[]>();
-    this.getAll().forEach(post => {
-      const year = post.date.getFullYear();
-      if (!mapByYear.has(year)) {
-        mapByYear.set(year, []);
-      }
-      mapByYear.get(year)!.push(post);
-    });
-
-    return Array.from(mapByYear.entries())
-      .map(([year, posts]) => ({ year, posts }))
-      .toSorted(({ year: yearA }, { year: yearB }) => yearB - yearA);
-  }
 }
 
 const Posts = new PostService();
