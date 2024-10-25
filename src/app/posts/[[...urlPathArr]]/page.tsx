52cd4b44a8bf7981f6d8211ee3828bc20ac56a18
wsh-n <dev@wshin.me>
2024-10-26 04:11:04 +0900

    [#14] 포스트 계층 뷰 추가

--- src/app/posts/[[...urlPathArr]]/page.tsx
+++ src/app/posts/[[...urlPathArr]]/page.tsx
@@ -2,7 +2,7 @@ import Posts from "@/services/post.service";
 import { notFound } from "next/navigation";
 import { defaultDateFormatter } from "@/utils/date";
 import GiscusComment from "@/components/GiscusComment";
-import Link from "next/link";
+import PostHierarchyView from "@/components/PostHierarchyView";
 
 export default function PostPage({ params }: { params: PostPageStaticParams }) {
   const urlPath = getUrlPath(params.urlPathArr);
@@ -14,19 +14,10 @@ export default function PostPage({ params }: { params: PostPageStaticParams }) {
   return (
     <div>
       <div>
-        {parentPost && <Link href={`/posts/${parentPost.urlPath}`}>상위 포스트</Link>}
         <h2 className="text-2xl font-bold">{title}</h2>
         <p className="mt-2 text-sm font-medium text-neutral-500">{defaultDateFormatter(date)}</p>
       </div>
-      {childPosts.length > 0 && (
-        <ul>
-          {childPosts.map(childPost => (
-            <li key={childPost.urlPath}>
-              <Link href={`/posts/${childPost.urlPath}`}>{childPost.title}</Link>
-            </li>
-          ))}
-        </ul>
-      )}
+      <PostHierarchyView parentPost={parentPost} childPosts={childPosts} />
       <div
         className="prose mt-10 overflow-x-scroll break-words dark:prose-invert"
         dangerouslySetInnerHTML={{ __html: bodyHtml }}
