339d870fb784ee841047e5ab4dfd1c4b0c0469fd
wsh-n <dev@wshin.me>
2024-10-31 22:49:21 +0900

    sass 지원

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./globals.scss";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import config from "@/config";
